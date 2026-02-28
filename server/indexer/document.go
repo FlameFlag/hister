bc6da322581a83acdb31e63ac95c051d67e1f4e9
Adam Tauber <asciimoo@gmail.com>
2026-02-28 14:20:12 +0100

    [mod] organize indexer

---
+++ server/indexer/document.go
@@ -0,0 +1,111 @@
+package indexer
+
+import (
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/rs/zerolog/log"
+)
+
+type Document struct {
+	URL                string  `json:"url"`
+	Domain             string  `json:"domain"`
+	HTML               string  `json:"html"`
+	Title              string  `json:"title"`
+	Text               string  `json:"text"`
+	Favicon            string  `json:"favicon"`
+	Score              float64 `json:"score"`
+	Added              int64   `json:"added"`
+	faviconURL         string
+	processed          bool
+	skipSensitiveCheck bool
+}
+
+func (d *Document) extractHTML() error {
+	return Extract(d)
+}
+
+func (d *Document) DownloadFavicon(userAgent string) error {
+	if d.faviconURL == "" {
+		d.faviconURL = fullURL(d.URL, "/favicon.ico")
+	}
+	if strings.HasPrefix(d.faviconURL, "data:") {
+		d.Favicon = d.faviconURL
+		return nil
+	}
+	cli := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+	req, err := http.NewRequest("GET", d.faviconURL, nil)
+	req.Header.Set("User-Agent", userAgent)
+	if err != nil {
+		return err
+	}
+	resp, err := cli.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("invalid status code (%d)", resp.StatusCode)
+	}
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	d.Favicon = fmt.Sprintf("data:%s;base64,%s", resp.Header.Get("Content-Type"), base64.StdEncoding.EncodeToString(data))
+	return nil
+}
+
+func (d *Document) Process() error {
+	if d.processed {
+		return nil
+	}
+	if !d.skipSensitiveCheck && sensitiveContentRe != nil && sensitiveContentRe.MatchString(d.HTML) {
+		log.Debug().Msg("Matching sensitive content: " + strings.Join(sensitiveContentRe.FindAllString(d.HTML, -1), ","))
+		return ErrSensitiveContent
+	}
+	if d.URL == "" {
+		return errors.New("missing URL")
+	}
+	pu, err := url.Parse(d.URL)
+	if err != nil {
+		return err
+	}
+	if pu.Scheme == "" || pu.Host == "" {
+		return errors.New("invalid URL: missing scheme/host")
+	}
+	if pu.Fragment != "" {
+		pu.Fragment = ""
+		d.URL = pu.String()
+	}
+	d.Added = time.Now().Unix()
+	q := pu.Query()
+	qChange := false
+	for k := range q {
+		if k == "utm" || strings.HasPrefix(k, "utm_") {
+			qChange = true
+			q.Del(k)
+		}
+	}
+	if qChange {
+		pu.RawQuery = q.Encode()
+		d.URL = pu.String()
+	}
+	d.Domain = pu.Host
+	if err := d.extractHTML(); err != nil {
+		return err
+	}
+	d.Title = strings.ReplaceAll(sanitizer.Sanitize(d.Title), "&#34;", `"`)
+	d.processed = true
+	return nil
+}
--- server/indexer/indexer.go
+++ server/indexer/indexer.go
@@ -1,12 +1,9 @@
 package indexer
 
 import (
-	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
-	"net/http"
 	"net/url"
 	"os"
 	"regexp"
@@ -46,20 +43,6 @@ type Query struct {
 	DateFrom  int64  `json:"date_from"`
 	DateTo    int64  `json:"date_to"`
 	cfg       *config.Config
-}
-
-type Document struct {
-	URL                string  `json:"url"`
-	Domain             string  `json:"domain"`
-	HTML               string  `json:"html"`
-	Title              string  `json:"title"`
-	Text               string  `json:"text"`
-	Favicon            string  `json:"favicon"`
-	Score              float64 `json:"score"`
-	Added              int64   `json:"added"`
-	faviconURL         string
-	processed          bool
-	skipSensitiveCheck bool
 }
 
 type Results struct {
@@ -258,50 +241,6 @@ func GetByURL(u string) *Document {
 	return docFromHit(res.Hits[0])
 }
 
-func (d *Document) Process() error {
-	if d.processed {
-		return nil
-	}
-	if !d.skipSensitiveCheck && sensitiveContentRe != nil && sensitiveContentRe.MatchString(d.HTML) {
-		log.Debug().Msg("Matching sensitive content: " + strings.Join(sensitiveContentRe.FindAllString(d.HTML, -1), ","))
-		return ErrSensitiveContent
-	}
-	if d.URL == "" {
-		return errors.New("missing URL")
-	}
-	pu, err := url.Parse(d.URL)
-	if err != nil {
-		return err
-	}
-	if pu.Scheme == "" || pu.Host == "" {
-		return errors.New("invalid URL: missing scheme/host")
-	}
-	if pu.Fragment != "" {
-		pu.Fragment = ""
-		d.URL = pu.String()
-	}
-	d.Added = time.Now().Unix()
-	q := pu.Query()
-	qChange := false
-	for k := range q {
-		if k == "utm" || strings.HasPrefix(k, "utm_") {
-			qChange = true
-			q.Del(k)
-		}
-	}
-	if qChange {
-		pu.RawQuery = q.Encode()
-		d.URL = pu.String()
-	}
-	d.Domain = pu.Host
-	if err := d.extractHTML(); err != nil {
-		return err
-	}
-	d.Title = strings.ReplaceAll(sanitizer.Sanitize(d.Title), "&#34;", `"`)
-	d.processed = true
-	return nil
-}
-
 func Iterate(fn func(*Document)) {
 	q := query.NewMatchAllQuery()
 	resultNum := 20
@@ -349,45 +288,6 @@ func docFromHit(h *search.DocumentMatch) *Document {
 		d.Added = int64(t)
 	}
 	return d
-}
-
-func (d *Document) extractHTML() error {
-	return Extract(d)
-}
-
-func (d *Document) DownloadFavicon(userAgent string) error {
-	if d.faviconURL == "" {
-		d.faviconURL = fullURL(d.URL, "/favicon.ico")
-	}
-	if strings.HasPrefix(d.faviconURL, "data:") {
-		d.Favicon = d.faviconURL
-		return nil
-	}
-	cli := &http.Client{
-		Timeout: 10 * time.Second,
-	}
-	req, err := http.NewRequest("GET", d.faviconURL, nil)
-	req.Header.Set("User-Agent", userAgent)
-	if err != nil {
-		return err
-	}
-	resp, err := cli.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("invalid status code (%d)", resp.StatusCode)
-	}
-
-	data, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
-
-	d.Favicon = fmt.Sprintf("data:%s;base64,%s", resp.Header.Get("Content-Type"), base64.StdEncoding.EncodeToString(data))
-	return nil
 }
 
 func (q *Query) create() query.Query {
