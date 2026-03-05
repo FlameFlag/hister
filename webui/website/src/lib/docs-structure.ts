4c22074f4e98d66197701f72b23fdce075ca03d8
ISSOtm <me@eldred.fr>
2026-03-05 18:28:37 -0500

    Define docs categories colors in a single place

--- webui/website/src/lib/docs-structure.ts
+++ webui/website/src/lib/docs-structure.ts
@@ -1,19 +1,23 @@
 export interface DocsCategory {
 	name: string;
 	slugs: string[];
+	color: string;
 }
 
 export const docsStructure: DocsCategory[] = [
 	{
 		name: "Getting Started",
 		slugs: ["intro", "installing", "getting-started"],
+		color: "indigo",
 	},
 	{
 		name: "Reference",
 		slugs: ["configuration", "query-language"],
+		color: "teal",
 	},
 	{
 		name: "Deployment",
 		slugs: ["server-setup"],
+		color: "coral",
 	},
 ];
